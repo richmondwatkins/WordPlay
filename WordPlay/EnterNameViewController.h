d45e70b29c5ae1893d15f8f06ad08d3edfb48f94
Richmond Watkins <richmondwatkins@mac.com>
2014-09-30 14:57:09 -0500

    added EnterName view controller

---
+++ WordPlay/EnterNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterNameViewController.h
+//  WordPlay
+//
+//  Created by Richmond on 9/30/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterNameViewController : UIViewController
+
+@end
